b559e8dd25db44b487be938c3f20f14d9c0cb292
ImJongHoon <imjh990614@gmail.com>
2022-12-05 17:38:32 +0900

    JSP project & DB commection

--- src/main.java
+++ src/main.java
@@ -1,5 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class main {
     public static void main(String[] args){
+        String url = "jdbc:mysql://192.168.56.101:4567/Law_firm_db";
+        String userName = "ijh0614";
+        String password = "1234";
 
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, userName,password);
+            Statement stmt = con.createStatement();
+
+            con.close();
+        }catch(Exception e){ System.out.println(e);}
     }
 }
